afff5fa4c39aa8ca20fdb0432be3a6553cb2ae33
SuleMareVientu <66548690+SuleMareVientu@users.noreply.github.com>
2024-04-06 19:29:20 +0200

    Reverted to old method, fixes
    New method doesn't work with other user's saves

--- README.md
+++ README.md
@@ -3,16 +3,16 @@ PSP2 savedata redirector
 
 # What is this plugin
 
-It is a plugin that redirects savedata such as games to `ux0:/data/resavedata` or `host0:/data/resavedata` without pfs encryption.
+It is a plugin that redirects savedata such as games to `ux0:/resavedata` without pfs encryption.
 
 # Specification
 
 All game save data will be redirected to reSavedata folder.
 
-Example : `ux0:/data/resavedata/PCSG00001/`
+Example : `ux0:/resavedata/PCSG00001/`
 
 # Installation
-Put "resavedata.skprx" in `ux0:tai/` and add the following to your config.txt (under the KERNEL section, if you have one):  
+Put "resavedata.skprx" in `ux0:tai/` and add the following to your config.txt under the *KERNEL* section:  
 `ux0:tai/resavedata.skprx`  
 
 # Information
--- src/main.c
+++ src/main.c
@@ -4,108 +4,184 @@
  */
 
 #include <psp2kern/kernel/modulemgr.h>
+#include <psp2kern/kernel/sysmem.h>
 #include <psp2kern/kernel/sysclib.h>
-#include <psp2kern/kernel/debug.h>
+#include <psp2kern/kernel/sysroot.h>
+#include <psp2kern/io/fcntl.h>
 #include <psp2kern/io/stat.h>
+#include <psp2kern/fios2.h>
 #include <taihen.h>
-
 
 #define HookImport(module_name, library_nid, func_nid, func_name) taiHookFunctionImportForKernel(KERNEL_PID, &func_name ## _ref, module_name, library_nid, func_nid, func_name ## _patch)
 #define HookOffset(modid, seg_index, thumb, offset, func_name) taiHookFunctionOffsetForKernel(KERNEL_PID, &func_name ## _ref, modid, seg_index, offset, thumb, func_name ## _patch);
 
+#define RE_SAVEDATA_PATH "ux0:resavedata"
 
-char rs_path[0x40];
+int ksceSysrootGetSelfAuthInfo(SceUID pid, SceSelfAuthInfo *self_auth_info);
 
+int write_file(const char *path, const void *data, size_t size){
 
-tai_hook_ref_t sceAppMgrGetSavedataPath_ref;
-int sceAppMgrGetSavedataPath_patch(const char *app0_base, const char *titleid, int flags, const char *sfo_category, char *savedata_path, SceSize savedata_path_len){
+	SceUID fd;
 
-	int res;
+	fd = ksceIoOpen(path, SCE_O_WRONLY | SCE_O_CREAT | SCE_O_TRUNC, 0666);
+	if(fd < 0){
+		return fd;
+	}
+
+	ksceIoWrite(fd, data, size);
+	ksceIoClose(fd);
+
+	return 0;
+}
+
+const char sdslot_magic[] = {
+	'S', 'D', 'S', 'L',
+	0x0, 0x0, 0x0, 0x0,
+	0x0, 0x1, 0x0, 0x0,
+	0x0, 0x0, 0x0, 0x0
+};
+
+tai_hook_ref_t sceAppMgrInitSafemem_ref;
+int sceAppMgrInitSafemem_patch(SceUID pid, int a2, const char *savedata0_sce_sys_path, char *a4, SceSize safemem_size){
+
+	int res = 0;
+	SceUID memid;
+	void *mem_base = NULL;
 	SceIoStat stat;
+	char path[0x100], titleid[0x20], process_path_savedata0[0x20];
+	SceSelfAuthInfo auth_info;
 
-	res = TAI_CONTINUE(int, sceAppMgrGetSavedataPath_ref, app0_base, titleid, flags, sfo_category, savedata_path, savedata_path_len);
-	if(res >= 0 && strcmp(sfo_category, "gd") == 0){
-		snprintf(savedata_path, savedata_path_len, "%s/%s", rs_path, titleid);
+	memset(process_path_savedata0, 0, sizeof(process_path_savedata0));
 
-		if(ksceIoGetstat(savedata_path, &stat) == 0x80010002){ // SCE_ERROR_ERRNO_ENOENT
-			ksceIoMkdir(savedata_path, 0606);
+	ksceSysrootGetSelfAuthInfo(pid, &auth_info);
+
+	if((strncmp(titleid, "NPXS", 4) == 0) || (auth_info.program_authority_id == 0x2808000000000000)){
+		goto tai_continue;
+	}
+
+	snprintf(path, sizeof(path), "%s/%s", savedata0_sce_sys_path, "safemem.dat");
+	if(ksceIoGetstat(path, &stat) < 0){
+
+		memid = ksceKernelAllocMemBlock("ReSdslot", SCE_KERNEL_MEMBLOCK_TYPE_RW_UNK0, safemem_size, NULL);
+
+		ksceKernelGetMemBlockBase(memid, &mem_base);
+
+		write_file(path, mem_base, safemem_size);
+
+		ksceKernelFreeMemBlock(memid);
+	}
+ 
+	ksceFiosKernelOverlayResolveSync(pid, 1, "savedata0:", process_path_savedata0, sizeof(process_path_savedata0));
+
+	if(strncmp(process_path_savedata0, "savedata0:", 10) == 0){
+		goto tai_continue;
+	}
+
+	ksceKernelSysrootGetProcessTitleId(pid, titleid, sizeof(titleid));
+
+	snprintf(path, sizeof(path), "%s/", RE_SAVEDATA_PATH);
+	res = ksceIoGetstat(path, &stat);
+	if(res < 0)
+		ksceIoMkdir(path, 0666);
+
+	snprintf(path, sizeof(path), "%s/%s/", RE_SAVEDATA_PATH, titleid);
+	res = ksceIoGetstat(path, &stat);
+	if(res < 0)
+		ksceIoMkdir(path, 0666);
+
+	snprintf(path, sizeof(path), "%s/%s/sce_sys/", RE_SAVEDATA_PATH, titleid);
+	res = ksceIoGetstat(path, &stat);
+	if(res < 0)
+		ksceIoMkdir(path, 0666);
+
+	snprintf(path, sizeof(path), "%s/%s/sce_sys/%s", RE_SAVEDATA_PATH, titleid, "safemem.dat");
+	if(ksceIoGetstat(path, &stat) < 0){
+
+		memid = ksceKernelAllocMemBlock("ReSafemem", SCE_KERNEL_MEMBLOCK_TYPE_RW_UNK0, safemem_size, NULL);
+
+		ksceKernelGetMemBlockBase(memid, &mem_base);
+
+		write_file(path, mem_base, safemem_size);
+
+		ksceKernelFreeMemBlock(memid);
+	}
+
+	snprintf(path, sizeof(path), "%s/%s/sce_sys/%s", RE_SAVEDATA_PATH, titleid, "sdslot.dat");
+	if(ksceIoGetstat(path, &stat) < 0){
+
+		memid = ksceKernelAllocMemBlock("ReSdslot", SCE_KERNEL_MEMBLOCK_TYPE_RW_UNK0, 0x50000, NULL);
+
+		ksceKernelGetMemBlockBase(memid, &mem_base);
+
+		memcpy(mem_base, sdslot_magic, 16);
+
+		write_file(path, mem_base, 0x40400);
+
+		ksceKernelFreeMemBlock(memid);
+	}
+
+tai_continue:
+	return TAI_CONTINUE(int, sceAppMgrInitSafemem_ref, pid, a2, savedata0_sce_sys_path, a4, safemem_size);
+}
+
+tai_hook_ref_t ksceFiosKernelOverlayAddForProcess_ref;
+int ksceFiosKernelOverlayAddForProcess_patch(SceUID pid, SceFiosOverlay *overlay, SceFiosOverlayID *outID){
+
+	SceFiosOverlay loc_overlay;
+
+	if(strcmp(overlay->dst, "savedata0:") == 0){
+
+		memcpy(&loc_overlay, overlay, sizeof(loc_overlay));
+
+		char titleid[0x20];
+		ksceKernelSysrootGetProcessTitleId(pid, titleid, sizeof(titleid));
+
+		if(strcmp(titleid, "main") != 0){
+			snprintf(loc_overlay.src, sizeof(loc_overlay.src), "%s/%s/", RE_SAVEDATA_PATH, titleid);
+			overlay = &loc_overlay;
 		}
 	}
 
-	return res;
+	return TAI_CONTINUE(int, ksceFiosKernelOverlayAddForProcess_ref, pid, overlay, outID);
 }
 
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args){
 
-	int res;
-	SceIoStat stat;
 	tai_module_info_t info;
 	info.size = sizeof(info);
 
-	if(taiGetModuleInfoForKernel(KERNEL_PID, "SceAppMgr", &info) < 0){
+	if(taiGetModuleInfoForKernel(KERNEL_PID, "SceAppMgr", &info) < 0)
 		return SCE_KERNEL_START_FAILED;
-	}
-
-	do {
-		res = ksceIoGetstat("host0:/data", &stat);
-		if(res >= 0){
-			snprintf(rs_path, sizeof(rs_path), "%s/resavedata", "host0:/data");
-			break;
-		}
-
-		res = ksceIoGetstat("ux0:/data", &stat);
-		if(res >= 0){
-			snprintf(rs_path, sizeof(rs_path), "%s/resavedata", "ux0:/data");
-			break;
-		}
-	} while(0);
-
-	if(res < 0){
-		ksceDebugPrintf("Not found reSavedata base\n");
-		return SCE_KERNEL_START_FAILED;
-	}
-
-	res = ksceIoMkdir(rs_path, 0606);
-	if(res < 0 && res != 0x80010011){
-		ksceDebugPrintf("sceIoMkdir 0x%X\n", res);
-		return SCE_KERNEL_START_FAILED;
-	}
 
 	switch(info.module_nid){
-	case 0x94CEFE4B: // 3.55 retail
-	case 0xDFBC288C: // 3.57 retail
-		HookOffset(info.modid, 0, 1, 0x17b74, sceAppMgrGetSavedataPath);
+	case 0x94CEFE4B: // 3.55
+		HookOffset(info.modid, 0, 1, 0x2DF9C, sceAppMgrInitSafemem);
 		break;
-
-	case 0xDBB29DB7: // 3.60 retail
-	case 0xB5F8EA7C: // 3.61 retail
-		HookOffset(info.modid, 0, 1, 0x17904, sceAppMgrGetSavedataPath);
+	case 0xDBB29DB7: // 3.60
+		HookOffset(info.modid, 0, 1, 0x2E0C4, sceAppMgrInitSafemem);
 		break;
-
-	case 0x23B967C5: // 3.63 retail
-	case 0x1C9879D6: // 3.65 retail
-		HookOffset(info.modid, 0, 1, 0x17440, sceAppMgrGetSavedataPath);
+	case 0x1C9879D6: // 3.65
+		HookOffset(info.modid, 0, 1, 0x2E0AC, sceAppMgrInitSafemem);
 		break;
-
-	case 0x54E2E984: // 3.67 retail
-	case 0xC3C538DE: // 3.68 retail
-		HookOffset(info.modid, 0, 1, 0x1744c, sceAppMgrGetSavedataPath);
+	case 0x54E2E984: // 3.67
+	case 0xC3C538DE: // 3.68
+		HookOffset(info.modid, 0, 1, 0x2E0BC, sceAppMgrInitSafemem);
 		break;
-
-	case 0x321E4852: // 3.69 retail
-	case 0x700DA0CD: // 3.70 retail
-	case 0xF7846B4E: // 3.71 retail
-	case 0xA8E80BA8: // 3.72 retail
-	case 0xB299D195: // 3.73 retail
-	case 0x30007BD3: // 3.74 retail
-		HookOffset(info.modid, 0, 1, 0x17474, sceAppMgrGetSavedataPath);
+	case 0x321E4852: // 3.69
+	case 0x700DA0CD: // 3.70
+	case 0xF7846B4E: // 3.71
+	case 0xA8E80BA8: // 3.72
+	case 0xB299D195: // 3.73
+	case 0x30007BD3: // 3.74
+		HookOffset(info.modid, 0, 1, 0x2E0E4, sceAppMgrInitSafemem);
 		break;
-
 	default:
 		return SCE_KERNEL_START_FAILED;
 		break;
 	}
+
+	HookImport("SceAppMgr", 0x54D6B9EB, 0x17E65A1C, ksceFiosKernelOverlayAddForProcess);
 
 	return SCE_KERNEL_START_SUCCESS;
 }
